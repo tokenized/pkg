7b66e2ea9cb580bbf0f9cb17b2ea687150841485
Curtis Ellis <curtis@nextcash.tech>
2021-02-18 23:25:37 -0700

    Remove zap logger.

--- logger/config.go
+++ logger/config.go
@@ -1,51 +1,49 @@
 package logger
 
-import (
-	"fmt"
-)
-
 // Config defines the logging configuration for the context it is attached to.
 type Config struct {
+	Main               systemConfig
 	Active             systemConfig
-	Main               *systemConfig
-	IncludedSubSystems map[string]bool          // If true, log in main log
-	SubSystems         map[string]*systemConfig // SubSystem specific loggers
+	IncludedSubSystems map[string]bool         // If true, log in main log
+	SubSystems         map[string]systemConfig // SubSystem specific loggers
+}
+
+func (c Config) Copy() Config {
+	result := c
+	result.IncludedSubSystems = make(map[string]bool)
+	for k, v := range c.IncludedSubSystems {
+		result.IncludedSubSystems[k] = v
+	}
+	result.SubSystems = make(map[string]systemConfig)
+	for k, v := range c.SubSystems {
+		result.SubSystems[k] = v
+	}
+	return result
 }
 
 // NewConfig creates a new config with the specified values.
-func NewConfig(isDevelopment, isText bool, filePath string) *Config {
+func NewConfig(isDevelopment, isText bool, filePath string) Config {
 	result := Config{
 		IncludedSubSystems: make(map[string]bool),
-		SubSystems:         make(map[string]*systemConfig),
+		SubSystems:         make(map[string]systemConfig),
 	}
 
 	var err error
 	result.Main, err = newSystemConfig(isDevelopment, isText, filePath)
 	if err != nil {
-		fmt.Printf("Failed to create log config : %s\n", err)
-		return nil
+		return result
 	}
 
-	result.Active = *result.Main
-	return &result
+	result.Active = result.Main.Copy()
+	return result
 }
 
 // NewEmptyConfig creates a new config that doesn't log.
-func NewEmptyConfig() *Config {
-	result := Config{
+func NewEmptyConfig() Config {
+	return Config{
 		IncludedSubSystems: make(map[string]bool),
-		SubSystems:         make(map[string]*systemConfig),
+		SubSystems:         make(map[string]systemConfig),
 	}
-
-	var err error
-	result.Main, err = newEmptySystemConfig()
-	if err != nil {
-		fmt.Printf("Failed to create log config : %s\n", err)
-		return nil
-	}
-
-	result.Active = *result.Main
-	return &result
 }
 
 // EnableSubSystem enables a subsytem to log to the main log
---
+++ logger/fields.go
@@ -0,0 +1,254 @@
+package logger
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/tokenized/pkg/json"
+)
+
+type Field interface {
+	Name() string
+	ValueJSON() string
+}
+
+type StringField struct {
+	name  string
+	value string
+}
+
+func (f StringField) Name() string {
+	return f.name
+}
+
+func (f StringField) ValueJSON() string {
+	return strconv.Quote(f.value)
+}
+
+func String(name string, value string) *StringField {
+	return &StringField{
+		name:  name,
+		value: value,
+	}
+}
+
+type JSONField struct {
+	name  string
+	value json.Marshaler
+}
+
+func (f JSONField) Name() string {
+	return f.name
+}
+
+func (f JSONField) ValueJSON() string {
+	b, err := f.value.MarshalJSON()
+	if err != nil {
+		return fmt.Sprintf("\"JSON Convert Failed : %s\"", err)
+	}
+	return string(b)
+}
+
+func Marshaler(name string, value json.Marshaler) *JSONField {
+	return &JSONField{
+		name:  name,
+		value: value,
+	}
+}
+
+type StringerField struct {
+	name  string
+	value fmt.Stringer
+}
+
+func (f StringerField) Name() string {
+	return f.name
+}
+
+func (f StringerField) ValueJSON() string {
+	return strconv.Quote(f.value.String())
+}
+
+func Stringer(name string, value fmt.Stringer) *StringerField {
+	return &StringerField{
+		name:  name,
+		value: value,
+	}
+}
+
+type IntField struct {
+	name  string
+	value int64
+}
+
+func (f IntField) Name() string {
+	return f.name
+}
+
+func (f IntField) ValueJSON() string {
+	return fmt.Sprintf("%d", f.value)
+}
+
+func Int(name string, value int) *IntField {
+	return &IntField{
+		name:  name,
+		value: int64(value),
+	}
+}
+
+func Int8(name string, value int8) *IntField {
+	return &IntField{
+		name:  name,
+		value: int64(value),
+	}
+}
+
+func Int16(name string, value int16) *IntField {
+	return &IntField{
+		name:  name,
+		value: int64(value),
+	}
+}
+
+func Int32(name string, value int32) *IntField {
+	return &IntField{
+		name:  name,
+		value: int64(value),
+	}
+}
+
+func Int64(name string, value int64) *IntField {
+	return &IntField{
+		name:  name,
+		value: value,
+	}
+}
+
+type UintField struct {
+	name  string
+	value uint64
+}
+
+func (f UintField) Name() string {
+	return f.name
+}
+
+func (f UintField) ValueJSON() string {
+	return fmt.Sprintf("%d", f.value)
+}
+
+func Uint(name string, value uint) *UintField {
+	return &UintField{
+		name:  name,
+		value: uint64(value),
+	}
+}
+
+func Uint8(name string, value uint8) *UintField {
+	return &UintField{
+		name:  name,
+		value: uint64(value),
+	}
+}
+
+func Uint16(name string, value uint16) *UintField {
+	return &UintField{
+		name:  name,
+		value: uint64(value),
+	}
+}
+
+func Uint32(name string, value uint32) *UintField {
+	return &UintField{
+		name:  name,
+		value: uint64(value),
+	}
+}
+
+func Uint64(name string, value uint64) *UintField {
+	return &UintField{
+		name:  name,
+		value: value,
+	}
+}
+
+type BoolField struct {
+	name  string
+	value bool
+}
+
+func (f BoolField) Name() string {
+	return f.name
+}
+
+func (f BoolField) ValueJSON() string {
+	return fmt.Sprintf("%t", f.value)
+}
+
+func Bool(name string, value bool) *BoolField {
+	return &BoolField{
+		name:  name,
+		value: value,
+	}
+}
+
+type Float32Field struct {
+	name  string
+	value float32
+}
+
+func (f Float32Field) Name() string {
+	return f.name
+}
+
+func (f Float32Field) ValueJSON() string {
+	return fmt.Sprintf("%f", f.value)
+}
+
+func Float32(name string, value float32) *Float32Field {
+	return &Float32Field{
+		name:  name,
+		value: value,
+	}
+}
+
+type Float64Field struct {
+	name  string
+	value float64
+}
+
+func (f Float64Field) Name() string {
+	return f.name
+}
+
+func (f Float64Field) ValueJSON() string {
+	return fmt.Sprintf("%f", f.value)
+}
+
+func Float64(name string, value float64) *Float64Field {
+	return &Float64Field{
+		name:  name,
+		value: value,
+	}
+}
+
+type FormatterField struct {
+	name   string
+	format string
+	values []interface{}
+}
+
+func (f FormatterField) Name() string {
+	return f.name
+}
+
+func (f FormatterField) ValueJSON() string {
+	return strconv.Quote(fmt.Sprintf(f.format, f.values...))
+}
+
+func Formatter(name string, format string, values ...interface{}) *FormatterField {
+	return &FormatterField{
+		name:   name,
+		values: values,
+	}
+}
