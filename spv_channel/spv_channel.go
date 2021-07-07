254bb71ef5608a3e47a68a273e8ffcb30e5359ec
Curtis Ellis <curtis@nextcash.tech>
2021-07-06 22:53:56 -0600

    Implement push notifications for SPV channels.

--- spv_channel/spv_channel.go
+++ spv_channel/spv_channel.go
@@ -4,13 +4,18 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/tokenized/pkg/json"
+	"github.com/tokenized/pkg/logger"
 
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
 )
 
@@ -51,11 +56,11 @@ func CreateChannel(ctx context.Context, baseURL, accountID, token string) (*Chan
 	return &response, nil
 }
 
-func PostMessage(ctx context.Context, baseURL, channelID, token,
-	message string) (*SPVMessage, error) {
+func PostMessage(ctx context.Context, baseURL, channelID, token string,
+	message interface{}) (*SPVMessage, error) {
 
 	response := &SPVMessage{}
-	if err := postWithToken(ctx, baseURL+"api/v1/channel/"+channelID, token, message,
+	if err := postWithToken(ctx, baseURL+"/api/v1/channel/"+channelID, token, message,
 		response); err != nil {
 		return nil, errors.Wrap(err, "http post")
 	}
@@ -66,7 +71,7 @@ func PostMessage(ctx context.Context, baseURL, channelID, token,
 func GetMessages(ctx context.Context, baseURL, channelID, token string,
 	unread bool) ([]*SPVMessage, error) {
 
-	url := baseURL + "api/v1/channel/" + channelID
+	url := baseURL + "/api/v1/channel/" + channelID
 	if unread {
 		url += "?unread=true"
 	} else {
@@ -79,6 +84,136 @@ func GetMessages(ctx context.Context, baseURL, channelID, token string,
 	}
 
 	return response, nil
+}
+
+func GetMaxMessageSequence(ctx context.Context, baseURL, channelID, token string) (int, error) {
+	url := baseURL + "/api/v1/channel/" + channelID
+
+	headers, err := headWithToken(ctx, url, token)
+	if err != nil {
+		return 0, errors.Wrap(err, "http head")
+	}
+
+	tag := headers.Get("ETag")
+	if len(tag) == 0 {
+		return 0, errors.New("Missing tag")
+	}
+
+	max, err := strconv.Atoi(tag)
+	if err != nil {
+		return 0, errors.Wrap(err, "parse tag")
+	}
+
+	return max, nil
+}
+
+func MarkMessages(ctx context.Context, baseURL, channelID, token string, sequence int,
+	read, older bool) error {
+
+	url := fmt.Sprintf("%s/api/v1/channel/%s/%d?older=%t", baseURL, channelID, sequence, older)
+
+	type RequestData struct {
+		Read bool `json:"read"`
+	}
+	requestData := RequestData{
+		Read: read,
+	}
+	if err := postWithToken(ctx, url, token, requestData, nil); err != nil {
+		return errors.Wrap(err, "http post")
+	}
+
+	return nil
+}
+
+// NotifyMessages starts a websocket for push notifications. `incoming` is the channel new messages
+// will be fed through. `interrupt` will stop listening if something is fed into it.
+func NotifyMessages(ctx context.Context, baseURL, channelID, token string,
+	incoming chan SPVMessage, interrupt chan interface{}) error {
+
+	url := fmt.Sprintf("%s/api/v1/channel/%s/notify", baseURL, channelID)
+	url = strings.ReplaceAll(url, "http", "ws")
+	// u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+
+	header := make(http.Header)
+	// Authorization: Bearer <token>
+	header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	conn, _, err := websocket.DefaultDialer.Dial(url, header)
+	if err != nil {
+		return errors.Wrap(err, "dial")
+	}
+	defer conn.Close()
+
+	// Listen for messages in separate thread.
+	done := make(chan interface{})
+	go func() {
+		for {
+			messageType, messageBytes, err := conn.ReadMessage()
+			if err != nil {
+				closeError, ok := err.(*websocket.CloseError)
+				if ok {
+					if closeError.Code != websocket.CloseNormalClosure &&
+						closeError.Code != websocket.CloseGoingAway {
+						logger.Info(ctx, "Non-normal websocket close message received : %s", err)
+					}
+				} else {
+					logger.Info(ctx, "Failed to read websocket message : %s", err)
+					if err := conn.WriteMessage(websocket.CloseMessage,
+						websocket.FormatCloseMessage(websocket.CloseAbnormalClosure,
+							"read failed")); err != nil {
+						logger.Warn(ctx, "Failed to send abnormal websocket close : %s", err)
+					}
+				}
+
+				break
+			}
+
+			if messageType != websocket.TextMessage {
+				logger.Info(ctx, "Wrong message type : got %d, want %d", messageType,
+					websocket.TextMessage)
+				break
+			}
+
+			logger.Info(ctx, "Received : %s", string(messageBytes))
+
+			var message SPVMessage
+			if err := json.Unmarshal(messageBytes, &message); err != nil {
+				logger.Info(ctx, "Failed to json unmarshal message : %s", err)
+				break
+			}
+
+			incoming <- message
+		}
+
+		close(incoming)
+		close(done)
+	}()
+
+	// Wait for interrupt or for listening to finish.
+	select {
+	case <-done:
+		logger.Info(ctx, "Finished listening for messages")
+		return nil
+
+	case <-interrupt:
+		logger.Info(ctx, "Listening stopped")
+
+		// Cleanly close the connection by sending a close message and then waiting (with timeout)
+		// for the server to close the connection.
+		if err := conn.WriteMessage(websocket.CloseMessage,
+			websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
+			return errors.Wrap(err, "send close")
+		}
+
+		select {
+		case <-done:
+			logger.Info(ctx, "Finished listening for messages")
+		case <-time.After(time.Second * 3):
+			logger.Info(ctx, "Server didn't close connection")
+		}
+
+		return nil
+	}
 }
 
 // postWithToken sends a request to the HTTP server using the POST method with an authentication
@@ -96,26 +231,32 @@ func postWithToken(ctx context.Context, url, token string, request, response int
 		Transport: transport,
 	}
 
-	var b []byte
-	if s, ok := request.(string); ok {
-		// request is already a json string, not an object to convert to json
-		b = []byte(s)
-	} else {
-		bt, err := json.Marshal(request)
-		if err != nil {
-			return errors.Wrap(err, "marshal request")
+	var r io.Reader
+	if request != nil {
+		var b []byte
+		if s, ok := request.(string); ok {
+			// request is already a json string, not an object to convert to json
+			b = []byte(s)
+		} else {
+			bt, err := json.Marshal(request)
+			if err != nil {
+				return errors.Wrap(err, "marshal request")
+			}
+			b = bt
 		}
-		b = bt
+		r = bytes.NewReader(b)
 	}
 
-	httpRequest, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(b))
+	httpRequest, err := http.NewRequestWithContext(ctx, http.MethodPost, url, r)
 	if err != nil {
 		return errors.Wrap(err, "create request")
 	}
 
 	// Authorization: Bearer <token>
 	httpRequest.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-	httpRequest.Header.Add("Content-Type", "application/json")
+	if request != nil {
+		httpRequest.Header.Add("Content-Type", "application/json")
+	}
 
 	httpResponse, err := client.Do(httpRequest)
 	if err != nil {
@@ -183,4 +324,41 @@ func getWithToken(ctx context.Context, url, token string, response interface{}) 
 	}
 
 	return nil
+}
+
+// headWithToken sends a request to the HTTP server using the HEAD method with an authentication
+// bearer token header.
+func headWithToken(ctx context.Context, url, token string) (*http.Header, error) {
+	var transport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+
+	var client = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: transport,
+	}
+
+	httpRequest, err := http.NewRequestWithContext(ctx, http.MethodHead, url, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "create request")
+	}
+
+	httpRequest.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	httpResponse, err := client.Do(httpRequest)
+	if err != nil {
+		return nil, errors.Wrap(err, "http post")
+	}
+
+	if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		if httpResponse.StatusCode == 404 {
+			return nil, errors.Wrap(ErrHTTPNotFound, httpResponse.Status)
+		}
+		return nil, fmt.Errorf("%v %s", httpResponse.StatusCode, httpResponse.Status)
+	}
+
+	return &httpResponse.Header, nil
 }
--- spv_channel/spv_channel_test.go
+++ spv_channel/spv_channel_test.go
@@ -3,5 +3,139 @@ package spv_channel
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
+	"time"
 )
+
+const rootURL = "http://localhost:8080"
+
+func TestCreateChannel(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	channel, err := CreateChannel(ctx, rootURL, "d695a715-e6c6-4ea1-a501-3eadc83055e0",
+		"87b587e9-98cd-43d2-9e8d-fc85ee714177")
+	if err != nil {
+		t.Fatalf("Failed to create channel : %s", err)
+	}
+
+	js, err := json.MarshalIndent(channel, "", "  ")
+	if err != nil {
+		t.Fatalf("Failed to marshal channel : %s", err)
+	}
+
+	t.Logf("Channel : %s", js)
+}
+
+func TestPostMessage(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	type MessageData struct {
+		Data string `json:"data"`
+	}
+
+	messageData := MessageData{Data: "Some test data"}
+
+	message, err := PostMessage(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+		"480bc60b-c779-450a-8c0b-854cbe856a92", messageData)
+	if err != nil {
+		t.Fatalf("Failed to post message : %s", err)
+	}
+
+	js, err := json.MarshalIndent(message, "", "  ")
+	if err != nil {
+		t.Fatalf("Failed to marshal message : %s", err)
+	}
+
+	t.Logf("Message : %s", js)
+}
+
+func TestGetMessage(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	messages, err := GetMessages(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+		"d4deef7c-cc6d-4e0a-9f1f-e7e6687d8bfd", true)
+	if err != nil {
+		t.Fatalf("Failed to get messages : %s", err)
+	}
+
+	js, err := json.MarshalIndent(messages, "", "  ")
+	if err != nil {
+		t.Fatalf("Failed to marshal messages : %s", err)
+	}
+
+	t.Logf("Messages : %s", js)
+}
+
+func TestGetMaxMessageSequence(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	max, err := GetMaxMessageSequence(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+		"d4deef7c-cc6d-4e0a-9f1f-e7e6687d8bfd")
+	if err != nil {
+		t.Fatalf("Failed to get max message sequence : %s", err)
+	}
+
+	t.Logf("Max Sequence : %d", max)
+}
+
+func TestMarkMessages(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	if err := MarkMessages(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+		"d4deef7c-cc6d-4e0a-9f1f-e7e6687d8bfd", 5, false, true); err != nil {
+		t.Fatalf("Failed to mark messages : %s", err)
+	}
+}
+
+func TestNotifyMessages(t *testing.T) {
+	t.Skip()
+	ctx := context.Background()
+
+	incoming := make(chan SPVMessage)
+	interrupt := make(chan interface{})
+
+	// Receive messages on incoming
+	go func() {
+		for msg := range incoming {
+			js, err := json.MarshalIndent(msg, "", "  ")
+			if err != nil {
+				t.Fatalf("Failed to marshal message : %s", err)
+			}
+
+			t.Logf("Received : %s", js)
+		}
+	}()
+
+	// Wait 12 seconds, then send interrupt from another thread
+	go func() {
+		time.Sleep(12 * time.Second)
+		t.Logf("Sending interrupt")
+		interrupt <- true
+	}()
+
+	// Send a message every second.
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			t.Logf("Sending message %d", i)
+			_, err := PostMessage(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+				"480bc60b-c779-450a-8c0b-854cbe856a92", fmt.Sprintf("Test message %d", i))
+			if err != nil {
+				t.Fatalf("Failed to post message : %s", err)
+			}
+		}
+	}()
+
+	if err := NotifyMessages(ctx, rootURL, "6f5a5fe3-bf66-4aac-a753-8e33bb77ee99",
+		"d4deef7c-cc6d-4e0a-9f1f-e7e6687d8bfd", incoming, interrupt); err != nil {
+		t.Fatalf("Failed to notify messages : %s", err)
+	}
+
+	t.Logf("Finished NotifyMessages")
+}
