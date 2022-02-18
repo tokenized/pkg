ad628b45dc4b063810b67eeb2357eb4e76cb18b8
Curtis Ellis <curtis@nextcash.tech>
2022-02-17 17:28:05 -0700

    Update logger elapsed format.

--- logger/elapsed.go
+++ logger/elapsed.go
@@ -6,12 +6,17 @@ import (
 )
 
 // Elapsed write elapsed time in milliseconds to the Logger.
-// Must be called with "defer" in front of it and with time.Now() as the time.
+// Should be called at the beginning of a function with "defer" in front of it and with time.Now()
+// as the "start" time.
 func Elapsed(ctx context.Context, start time.Time, format string, values ...interface{}) {
-	// get the elapsed time in milliseconds
-	ms := float64(time.Since(start).Nanoseconds()) / float64(time.Millisecond)
+	LogDepthWithFields(ctx, LevelInfo, GetCaller(1), []Field{
+		MillisecondsFromNano("elapsed_ms", time.Since(start).Nanoseconds()),
+	}, format, values...)
+}
 
-	values = append(values, ms)
+func ElapsedWithFields(ctx context.Context, start time.Time, fields []Field, format string,
+	values ...interface{}) {
 
-	LogDepth(ctx, LevelInfo, GetCaller(1), format+" : %0.3f ms", values...)
+	fields = append(fields, MillisecondsFromNano("elapsed_ms", time.Since(start).Nanoseconds()))
+	LogDepthWithFields(ctx, LevelInfo, GetCaller(1), fields, format, values...)
 }
