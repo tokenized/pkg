7ce964107bd034e743ccac6625824756f54777a1
Curtis Ellis <curtis@nextcash.tech>
2022-08-08 15:45:34 -0600

    Add json envelope wrap function.

--- json_envelope/json_envelope.go
+++ json_envelope/json_envelope.go
@@ -2,8 +2,9 @@ package json_envelope
 
 import (
 	"crypto/sha256"
-	"errors"
+	"encoding/json"
 
+	"github.com/pkg/errors"
 	"github.com/tokenized/pkg/bitcoin"
 )
 
@@ -19,6 +20,29 @@ type JSONEnvelope struct {
 	PublicKey *bitcoin.PublicKey `bsor:"3" json:"publicKey"`
 	Encoding  string             `bsor:"4" json:"encoding"`
 	MimeType  string             `bsor:"5" json:"mimetype"`
+}
+
+func WrapJSON(key bitcoin.Key, payloadStruct interface{}) (*JSONEnvelope, error) {
+	js, err := json.Marshal(payloadStruct)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal payload")
+	}
+
+	hash := bitcoin.Hash32(sha256.Sum256(js))
+	publicKey := key.PublicKey()
+
+	sig, err := key.Sign(hash)
+	if err != nil {
+		return nil, errors.Wrap(err, "sign")
+	}
+
+	return &JSONEnvelope{
+		Payload:   string(js),
+		Signature: &sig,
+		PublicKey: &publicKey,
+		Encoding:  "UTF-8",
+		MimeType:  "application/json",
+	}, nil
 }
 
 // Verify verifies the signature is valid.
